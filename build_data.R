d1b0d3ad59c7f63a68b37e94fc00abce6a1e0f52
Andrew Heiss <andrew@andrewheiss.com>
2025-10-20 13:57:25 -0400

    Add instructions for installing fonts on Posit Cloud

--- build_data.R
+++ build_data.R
@@ -10,3 +10,13 @@ quarto::quarto_render(
   "answers.qmd",
   output_format = c("html", "typst")
 )
+
+# How to install Lato on Posit Cloud ----
+
+# 1. Download the font from Google Fonts: 
+#    <https://fonts.google.com/download?family=Lato>
+# 2. Upload the .zip file to Posit Cloud
+# 3. Rename the uploaded/unzipped folder to .fonts (or whatever, really; 
+#    the . prefix keeps things clean)
+# 4. Run this in the terminal to register the fonts and rebuilt the font cache:
+#    fc-cache -fv ~/.fonts
